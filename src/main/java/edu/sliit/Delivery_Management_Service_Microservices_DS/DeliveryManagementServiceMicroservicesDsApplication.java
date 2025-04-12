0917002042e5bc22752ff7cfc0c3649ac2438284
ArunaluB <it22515612@my.sliit.lk>
2025-04-12 21:06:49 +0530

    project inzizer

---
+++ src/main/java/edu/sliit/Delivery_Management_Service_Microservices_DS/DeliveryManagementServiceMicroservicesDsApplication.java
@@ -0,0 +1,13 @@
+package edu.sliit.Delivery_Management_Service_Microservices_DS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManagementServiceMicroservicesDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryManagementServiceMicroservicesDsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sliit/Delivery_Management_Service_Microservices_DS/DeliveryManagementServiceMicroservicesDsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sliit.Delivery_Management_Service_Microservices_DS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryManagementServiceMicroservicesDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
